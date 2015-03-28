3efb7c4eeb717915d11a548a8ab591130b431d0b
Avinash B <abaldeo@users.noreply.github.com>
2015-03-28 18:29:42 -0400

    Update rollingcurlx.class.php
    added namespace

--- rollingcurlx.class.php
+++ rollingcurlx.class.php
@@ -5,6 +5,7 @@
 **
 **  a fork of Rolling Curl (http://code.google.com/p/rolling-curl/)
 */
+namespace marcushat\RollingCurlX;
 Class RollingCurlX {
     private $_maxConcurrent = 0; //max. number of simultaneous connections allowed
     private $_options = []; //shared cURL options
